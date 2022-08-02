b018d68b252b6989b41cb374056b14c61e8ad916
Chinna <ChinnaAlupuru@gmail.com>
2022-08-02 10:03:39 +0530

    firstCommit

---
+++ src/main/java/com/smart/SpringJenkins/SpringJenkinsApplication.java
@@ -0,0 +1,18 @@
+package com.smart.SpringJenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("Springboot application has started");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
