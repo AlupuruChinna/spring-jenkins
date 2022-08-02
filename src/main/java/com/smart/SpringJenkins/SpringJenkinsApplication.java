d54df72869011e40d9147599ca9686198e5587f5
ChinnaAlupuru@gmail.com <ChinnaAlupuru@gmail.com>
2022-08-02 10:46:26 +0530

    scommit

--- src/main/java/com/smart/SpringJenkins/SpringJenkinsApplication.java
+++ src/main/java/com/smart/SpringJenkins/SpringJenkinsApplication.java
@@ -11,7 +11,7 @@ public class SpringJenkinsApplication {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
 	public static void main(String[] args) {
-		logger.info("Springboot application has started");
+		logger.info("Springboot application has started..");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
